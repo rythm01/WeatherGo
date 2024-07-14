3e32802b411fd4f2e8dcaa1d28272448c8c703ae
Ridham <115993280+rythm01@users.noreply.github.com>
2024-07-14 19:33:54 +0530

    added constants

---
+++ constants/index.js
@@ -0,0 +1,19 @@
+export const API_KEY = '725c894421fb4a3fbc9132549241407'
+export const weatherImages = {
+  'Moderate rain': require('../assets/images/moderaterain.png'),
+  'Overcast': require('../assets/images/cloud.png'),
+  'Patchy rain possible': require('../assets/images/moderaterain.png'),
+  'Partly Cloudy': require('../assets/images/partlycloudy.png'),
+  'Patchy rain nearby': require('../assets/images/Patchyrainnearby.jpg'),
+  'Sunny': require('../assets/images/sun.png'),
+  'Clear': require('../assets/images/sun.png'),
+  'Cloudy': require('../assets/images/cloud.png'),
+  'Light rain': require('../assets/images/moderaterain.png'),
+  'Moderate rain at times': require('../assets/images/moderaterain.png'),
+  'Heavy rain': require('../assets/images/heavyrain.png'),
+  'Heavy rain at times': require('../assets/images/heavyrain.png'),
+  'Moderate or heavy freezing rain': require('../assets/images/heavyrain.png'),
+  'Moderate or heavy rain shower': require('../assets/images/heavyrain.png'),
+  'Moderate or heavy rain with thunder': require('../assets/images/heavyrain.png'),
+  'Other': require('../assets/images/moderaterain.png'),
+}
